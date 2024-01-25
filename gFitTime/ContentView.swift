25aadecc058a2e47d85e97a77e10fe50bd9508c5
Giacomo Angione <angi1g@gmail.com>
2024-01-25 18:05:43 +0100

    Initial Commit

--- gFitTime/ContentView.swift
+++ gFitTime/ContentView.swift
@@ -15,6 +15,7 @@ struct ContentView: View {
     
     var body: some View {
         ZStack {
+            // background
             Color("colorCF")
                 .ignoresSafeArea()
             VStack {
